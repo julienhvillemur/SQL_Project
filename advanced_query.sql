59dd0a17f59cf3a08002a4d16444e0fca1153a62
julienhvillemur <julienhvillemur@gmail.com>
2024-10-27 18:49:22 +0000

    This adds: CTE for left outer join, window function for running total payment per customer & new CSV output file from the query.

--- advanced_query.sql
+++ advanced_query.sql
@@ -1,9 +1,24 @@
--- Calculate total amount paid per customer
+-- Calculate total amount paid per customer as CTE
+WITH cte AS (SELECT
+    c.customer_id,
+    c.first_name,
+    c.last_name,
+    p.amount,
+    p.payment_date
+FROM
+    customer AS c
+LEFT JOIN
+    payment AS p
+ON
+    c.customer_id = p.customer_id)
+
+--Use window function to calculate running total per customer
 SELECT
     customer_id,
     first_name,
     last_name,
-    amount
+    amount,
+    TO_CHAR(payment_date, 'YYYY-MM-DD HH24:MI:SS') AS payment_date,
+    SUM(amount) OVER (PARTITION BY customer_id ORDER BY payment_date) AS running_total
 FROM
-    customer
-    
+    cte;
